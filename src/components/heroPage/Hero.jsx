ed0b568612712131e0413590adcf55a09069368b
Anish-649 <kumaranish4098@gmail.com>
2025-10-01 22:21:39 +0530

    her page bg change , second page update and fifth page image box change

--- src/components/heroPage/Hero.jsx
+++ src/components/heroPage/Hero.jsx
@@ -1,11 +1,12 @@
 import "./hero.css"
-
+import bg_logo from "../../assets/bg_logo.png";
 
 export default function Hero() {
   return (
     <div className="hero">
-      
-      <h1> ASIA’S FIRST <br/>AI-POWERED<br/></h1>
+      <img className="hero-bg" src={bg_logo} alt="backgromund color"/>
+      <h1> ASIA’S FIRST <br/>AI-POWERED<br/> PR MARKETING AGENCY</h1>
+      <h3>The Leading PR Agency with Proven Success.</h3>
     </div>
   )
 }
--- src/components/secondPage2/SecondPage2.jsx
+++ src/components/secondPage2/SecondPage2.jsx
@@ -9,11 +9,7 @@ export default function SecondPage2() {
         <CurveLine />
         <div className="secondPage2-title">
           <h1>
-            WE GAIN TRUST BY
-            <br />
-            WORKING
-            <br />
-            EFFICIENTLY
+            __WE GAIN <span>TRUST</span> BY WORKING <span>EFFICIENTLY</span>__
           </h1>
         </div>
         <div className="gainTrust">
@@ -29,8 +25,13 @@ export default function SecondPage2() {
             <h1>12K+</h1>
             <h2>PR works Completed</h2>
           </div>
+        
+           
         </div>
-        <div className="teamMember"></div>
+          <div className="teamMember workBox">
+         <p>At Fox Wing Media LLP, we believe public relations is more than press releases. It’s the art of shaping how your story is heard, how your reputation is built, and how your brand earns trust in a crowded digital age.</p>
+       <p>We are an award-winning PR and marketing team, powered by PhD-holder marketers, seasoned strategists, and AI-driven innovation. With a blend of academic depth and industry expertise, we create PR that not only boosts visibility but also drives measurable business outcomes.</p>
+        </div>
       </div>
     );
 }
